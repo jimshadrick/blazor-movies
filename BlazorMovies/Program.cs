63008c979eec7af2c5b49bf8922cac7899266152
Jim Shadrick <jshadrick@live.com>
2023-12-04 17:18:29 -0600

    Add Movie cards.

--- BlazorMovies/Program.cs
+++ BlazorMovies/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorMovies.Components.Ui;
+using BlazorMovies.Components;
 using BlazorMovies.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
